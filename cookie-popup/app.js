ff9d0f9dd62be2bede358335283beaa78ff679a0
maxts0gt <bazarsadtsogtjargal@gmail.com>
2022-04-26 16:20:33 +0900

    finishing touch for cookie popup

--- cookie-popup/app.js
+++ cookie-popup/app.js
@@ -1,1 +1,10 @@
-gsap.fromTo('.text', { opacity: 0, y: 100 }, { opacity: 1, y: 0, duration: 1 });
+const tl = gsap.timeline({ defaults: { duration: 0.75, ease: 'elastic.out' } });
+
+tl.fromTo('.cookie-container', { scale: 0 }, { scale: 1, duration: 1.5 });
+tl.fromTo(
+	'.cookie',
+	{ opacity: 0, x: -50, rotation: '-45deg' },
+	{ opacity: 1, x: 0, rotation: '0deg' }
+);
+
+tl.fromTo('.text', { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, '<');
